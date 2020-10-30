04deb2b7272b21c4e08320f3560facbfcfcd8093
Christian Dueñas <46909500+ChristianD37@users.noreply.github.com>
2020-10-30 12:25:39 -0700

    Add files

---
+++ Physics/player.py
@@ -0,0 +1,73 @@
+import pygame
+from spritesheet import Spritesheet
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = Spritesheet('spritesheet.png').parse_sprite('chick.png')
+        self.rect = self.image.get_rect()
+        self.LEFT_KEY, self.RIGHT_KEY, self.FACING_LEFT = False, False, False
+        self.is_jumping, self.on_ground = False, False
+        self.gravity, self.friction = .35, -.12
+        self.position, self.velocity = pygame.math.Vector2(0, 0), pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, self.gravity)
+
+    def draw(self, display):
+        display.blit(self.image, (self.rect.x, self.rect.y))
+
+    def update(self, dt):
+        self.horizontal_movement(dt)
+        self.vertical_movement(dt)
+
+    def horizontal_movement(self, dt):
+        self.acceleration.x = 0
+        if self.LEFT_KEY:
+            self.acceleration.x -= .3
+        elif self.RIGHT_KEY:
+            self.acceleration.x += .3
+        self.acceleration.x += self.velocity.x * self.friction
+        self.velocity.x += self.acceleration.x * dt
+        self.limit_velocity(4)
+        self.position.x += self.velocity.x * dt + (self.acceleration.x * .5) * (dt * dt)
+        self.rect.x = self.position.x
+
+    def vertical_movement(self, dt):
+        self.velocity.y += self.acceleration.y * dt
+        if self.velocity.y > 7: self.velocity.y = 7
+        self.position.y += self.velocity.y * dt + (self.acceleration.y * .5) * (dt * dt)
+        if self.position.y > 128:
+            self.on_ground = True
+            self.velocity.y = 0
+            self.position.y = 128
+        self.rect.bottom = self.position.y
+
+
+    def limit_velocity(self, max_vel):
+        min(-max_vel, max(self.velocity.x, max_vel))
+        if abs(self.velocity.x) < .01: self.velocity.x = 0
+
+    def jump(self):
+        if self.on_ground:
+            self.is_jumping = True
+            self.velocity.y -= 8
+            self.on_ground = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Physics/spritesheet.py
@@ -0,0 +1,32 @@
+import pygame
+import json
+
+
+class Spritesheet:
+    def __init__(self, filename):
+        self.filename = filename
+        self.sprite_sheet = pygame.image.load(filename).convert()
+        self.meta_data = self.filename.replace('png', 'json')
+        with open(self.meta_data) as f:
+            self.data = json.load(f)
+        f.close()
+
+
+
+    def get_sprite(self, x, y, w, h):
+        sprite = pygame.Surface((w, h))
+        sprite.set_colorkey((0,0,0))
+        sprite.blit(self.sprite_sheet,(0, 0),(x, y, w, h))
+        return sprite
+
+    def parse_sprite(self, name):
+        sprite = self.data['frames'][name]['frame']
+        x, y, w, h = sprite["x"], sprite["y"], sprite["w"], sprite["h"]
+        image = self.get_sprite(x, y, w, h)
+        return image
+
+
+
+
+
+
